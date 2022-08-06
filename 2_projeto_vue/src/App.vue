1d251987fe7c85d0efc349a5b08eaca3d922f509
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-08-06 00:03:49 -0300

    vue

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <PrimeiroCompnente />
+</template>
+
+<script>
+  import PrimeiroCompnente from './components/PrimeiroCompnente.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      PrimeiroCompnente
+    }
+  }
+</script>
---
+++ 2_projeto_vue/src/components/PrimeiroCompnente.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Olá Vue!</h1>
+</template>
+
+<script>
+    export default {
+        name: 'PrimeiroComponent',
+        data() {
+            return {
+                nome: "Matheus"
+            }
+        }
+    }
+</script>
