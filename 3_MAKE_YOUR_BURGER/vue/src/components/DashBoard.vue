585f606a2820d457d52fb59b3cb140318a6b274e
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-08-14 00:04:55 -0300

    Vue

--- 3_MAKE_YOUR_BURGER/vue/src/components/DashBoard.vue
+++ 3_MAKE_YOUR_BURGER/vue/src/components/DashBoard.vue
@@ -23,10 +23,13 @@
                 </ul>
             </div>
             <div>
-                <select name="status" id="status">
+                <select name="status" id="status" @change="updateBurger($event, burger.id)">
                     <option value="">Selecione</option>
+                    <option v-for="s in status" :key="s.id" value="s.tipo" :selecte="burger.status == s.tipo">
+                        {{ s.tipo }}
+                    </option>
                 </select>
-                <button class="delete-btn">Cancelar</button>
+                <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
             </div>     
         </div>
        
@@ -53,9 +56,44 @@
 
             this.burgers = data;
 
-            // 
-          }   
+            this.getStatus(); 
+             
         },
+        async getStatus() {
+
+            const req = await fetch("http://localhost:3000/status")
+            
+            const data = await req.json();
+
+            this.status = data;
+
+            },
+            async deleteBurger(id) {
+               const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                    method: "DELETE"
+               });
+
+               const res = await req.json();
+
+               this.getPedidos();
+            },
+
+            async updateBurger(event, id) {
+                const option = event.target.value;
+
+                const dataJson = JSON.stringify({ status: option });
+
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "PATCH",
+                    headers: { "Content´Type": "application/json"}
+                });
+
+                const res = await req.json();
+
+
+            }
+        },
+
         mounted() {
             this.getPedidos();
         }
